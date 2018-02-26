fd77c89eea357d231e7efb850f64bf4916cf6067
mgctech <36328457+mgctech@users.noreply.github.com>
2018-02-26 19:20:50 +0000

    Add files via upload

---
+++ src/test/java/com/mgctech/elitepos/ElitePosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mgctech.elitepos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElitePosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
